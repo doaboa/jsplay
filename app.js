b68b03c0de038d94572705e02662b1a58e3e471d
MakerSquare Student <students@themakersquare.com>
2013-07-15 16:49:10 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//A simple pop-up
+// alert("What's your name?");
+
+
+//Getting input from a pop-up, setting it as a variable and printing it in cosole
+// var name = prompt("What's your name?", "put it here");
+// console.log("Your name is " + name + "\nCool,huh?");
+
+
+//validates input
